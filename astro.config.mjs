de69a5c106b3f8eaa7c5f4d1030d19a4381e5f6d
Josha Ho <joshaho@Joshas-Mac-mini.local>
2024-04-23 19:46:59 -0400

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://swingerasteps.com",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), 
+			icon(), 
+			mdx(), 
+			sitemap(), 		
+			partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),],
 });
+
